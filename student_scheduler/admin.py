2ad2b2fc489f9a31a41aa5b41f139bbd3302fad6
Saito-Yuki-3110 <yukinosuke5@icloud.com>
2022-12-03 12:25:33 +0900

    relate to migrate

--- student_scheduler/admin.py
+++ student_scheduler/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 # モデルをインポート
-from . models import Student
-from . models import Lesson
+from . models import Student, Lesson, Message
 
 # 管理ツールに登録
 Teacher = get_user_model()
 admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(Lesson)
+admin.site.register(Message)
